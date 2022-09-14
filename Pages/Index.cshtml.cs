51402f73b10c975a32496da68194be389b8dea72
Anamika Sharma <anasharm7@publicisgroupe.net>
2022-09-14 13:16:11 +0530

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using LocalSQLApp.Models;
+using LocalSQLApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalSQLApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            products = productService.GetProducts();
+        }
+    }
+}
